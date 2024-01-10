914e1164c559cd2d5341852fbc9c2860899157f3
Yusaf Saddiq <yusafsaddiq@gmail.com>
2024-01-10 05:58:21 +0000

    all nighter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { useRef, useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-// import fightersData from "../public/data/fighters.json";
 
+const capitaliseFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 function Navigation() {
   return (
@@ -16,14 +18,16 @@ function Header(props) {
   return (
     <header>
       <Navigation />
-      <h1>{props.name}</h1>
+      <h1 className='bg-gradient-to-r from-secondary1 to-secondary2 inline-block text-transparent bg-clip-text'>{props.name}</h1>
     </header>
   )
 }
 function Main() {
+  const today = new Date();
+  const currentYear = today.getFullYear();
   return (
     <div>
-      <p>Find out about MMA fighters.</p>
+      <p>Find out about currently ranked UFC fighters as of {today.toDateString()}.</p>
       <Form />
     </div>
   )
@@ -38,6 +42,7 @@ function Form() {
   return (
     <form id="search-form">
       <label for="searchInput">Enter a fighter name: </label>
+      <br />
       <input ref={nameInput} id="searchInput" placeholder="Fighter name" />
       <button>Search</button>
     </form>
@@ -55,7 +60,6 @@ function Data() {
 
   useEffect(() => {
     // Fetch data from local JSON file
-
     fetch(`/data/fighters.json`)
       .then((response) => response.json())
       .then((data) => setData(data))
@@ -63,28 +67,69 @@ function Data() {
 
   if (allFightersData) {
     return (
-      <div className='mx-auto flex flex-col flex-wrap justify-center items-center md:flex-row border border-red-700'>
-        {/* Render your data as needed */}
-        {allFightersData.fighters.map((fighter) => (
-          <div className='w-4/5 md:w-1/5 md:m-2 border border-slate-400 shadow-md hover:shadow-none rounded-lg hover:-translate-y-4 transition-transform' key={fighter.id}>
-            <Link to={`/profile/${fighter.id}`}>
-              <img alt={`Profile of ${fighter.name}`} className='max-w-full rounded-t-lg' src={fighter.img} height={150} />
-              {fighter.name}
-              <br />
-              {fighter.nickname}
-            </Link>
-
-            {/* Display other fighter details as needed */}
-          </div>
-        ))
-        }
-      </div >
+      <>
+        {/* <h2>Pound-for-Pound</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Pound-for-Pound" /> */}
+        <h2>Heavyweight</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Heavyweight" />
+        <h2>Light Heavyweight</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Light Heavyweight" />
+        <h2>Middleweight</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Middleweight" />
+        <h2>Welterweight</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Welterweight" />
+        <h2>Lightweight</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Lightweight" />
+        <h2>Featherweight</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Featherweight" />
+        <h2>Bantamweight</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Bantamweight" />
+        <h2>Flyweight</h2>
+        <Cards fightersData={allFightersData.fighters} weightClass="Flyweight" />
+      </>
     );
   } else {
     return (
       <h1>Loading...</h1>
-    )
+    );
   }
+}
+function Cards({ fightersData, weightClass }) {
+  const filteredFighters = fightersData.filter((fighter) => fighter.weight_class === weightClass);
+  return (
+    <div className='mx-auto flex flex-row flex-wrap gap-4 justify-center items-center border border-red-700'>
+      {filteredFighters.map((fighter) => (
+        <div className='w-2/5 md:w-52 bg-transparent hover:bg-blue-500 border border-black-400 hover:border-slate-400 rounded-lg hover:-translate-y-3 transition-transform p-2' key={fighter.id}>
+          <Link to={`/profile/${fighter.id}`}>
+            <div className='flex flex-col h-80'>
+              <div className='w-32 self-center border-red-500'>
+                <img alt={`Profile of ${fighter.name}`} className='max-w-full rounded-t-lg' src={fighter.img} />
+              </div>
+              <div className='text-left flex flex-col justify-start'>
+                <div className='flex flex-col'>
+                  <h4 className='leading-none'>{fighter.name}
+                  </h4>
+                  <div>
+                    <span className='bg-green-500 px-2 rounded-lg w-auto'>{fighter.wins}</span>
+                    <span className='bg-red-700 px-2 rounded-lg w-auto'>{fighter.losses}</span>
+                  </div>
+                  <p>{fighter.nickname ? `'${fighter.nickname}'` : fighter.nickname}</p>
+                </div>
+                <div className='border-t'>
+                  <p><span className='bg-green-500 px-2 rounded-lg'>{capitaliseFirstLetter(fighter.latestFight.result)}</span> - {fighter.latestFight.opponent}</p>
+                  <p>{fighter.latestFight.name}</p>
+                  <div className='flex flex-row items-center'>
+                    <p className='text-xs'>{fighter.latestFight.method}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Link>
+          {/* Display other fighter details as needed */}
+        </div>
+      ))}
+    </div>
+  );
 }
 export function Favourites() {
   return (
@@ -107,14 +152,52 @@ export function Profile() {
   }, [id]);
 
   if (fighterData) {
-    console.log('Done')
+    function FightCard({ arrayNum }) {
+      return (
+        <div className='md:w-3/12 border border-slate-700 p-4 bg-gradient-to-r from-primary1 to-primary2 rounded-lg'>
+          <div className='border-b'>
+            <a className='hover:underline bg-green-700 rounded-lg px-2 float-end' href={`https://www.sherdog.com${fighterData.fights[arrayNum].url}`} target='blank'>View fight card</a>
+            <h4>{fighterData.fights[arrayNum].name}</h4>
+            <p>{capitaliseFirstLetter(fighterData.fights[arrayNum].result)} - {fighterData.fights[arrayNum].method}</p>
+          </div>
+          <div></div>
+        </div>
+      )
+    }
     return (
       <>
         <Navigation />
         <section>
-          <h1>{fighterData.name}'s Profile Page.</h1>
-          <p>{fighterData.description}</p>
-        </section></>
+          <div style={{ backgroundImage: `url(${fighterData.img})` }} className='h-screen bg-contain bg-center bg-no-repeat flex flex-col justify-end items-center'>
+            <a href="#bio"><img className='hover:bg-gradient-to-r from-secondary1 to-secondary2 hover:-translate-y-3 transition-transform w-12 md:w-16 mb-6 border-t rounded-full text-white' src="/arrow_downward.svg" alt="" /></a>
+          </div>
+          <h1 className='text-center'>{fighterData.name}</h1>
+          <div className='flex flex-col md:flex-row gap-4'>
+            <div className='p-6 md:w-1/2 border border-slate-700 bg-gradient-to-r from-primary1 to-primary2 rounded-lg'>
+              <h3 id='bio'>Bio</h3>
+              <p>{fighterData.bio}</p>
+            </div>
+            <div className='p-6 md:w-1/2 border border-slate-700 bg-gradient-to-r from-primary1 to-primary2 rounded-lg'>
+              <h3>Stats</h3>
+              <div>
+                <p>Wins: {fighterData.wins.total}</p>
+                <p>Losses: {fighterData.losses.total}</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className='bg-gradient-to-r from-secondary1 to-secondary2 p-4'>
+            <h3>Recent fights</h3>
+            <div className='flex flex-col md:flex-row gap-4'>
+              <FightCard arrayNum="0" />
+              <FightCard arrayNum="1" />
+              <FightCard arrayNum="2" />
+              <FightCard arrayNum="2" />
+            </div>
+          </div>
+        </section>
+      </>
     )
   } else {
     return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary1: "#1e293b",
+        primary2: "#0f172a",
+        secondary1: "#ef4444",
+        secondary2: "#f97316",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ],
 }
 
